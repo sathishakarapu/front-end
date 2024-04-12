1ef0c3febdde14c48547e2a55c2691d99c5e593b
sathishakarapu <149458266+sathishakarapu@users.noreply.github.com>
2024-04-12 23:56:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>} />
+        <Route path='/' element={<Signup/>} />
         <Route path='/home' element={<Home />} />
       </Routes>
     </>
